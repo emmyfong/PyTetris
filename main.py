c9b9de32531c779e4ec4e6e0b409b52d70393d9e
Emmy <emmyfong@gmail.com>
2026-09-02 14:19:40 -0700

    Initial file setup

---
+++ main.py
@@ -0,0 +1,26 @@
+#App state and game entry -> keyboard input
+
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("cornflowerblue")
+
+    # RENDER YOUR GAME HERE
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
